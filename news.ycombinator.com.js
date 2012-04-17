5e92078e92eb6f6bdb24ae9741577bce0ae194ed
Reinis Ivanovs <dabas@untu.ms>
2012-04-17 18:39:34 +0300

    Move styles to .css file

--- news.ycombinator.com.js
+++ news.ycombinator.com.js
@@ -1,17 +1,6 @@
 (function($) {
     var $scores = $('span[id^=score]');
     var max_score = 0;
-    var style_text = 'td.subtext, span.comhead, td.title { \n\
-        color: rgba(0, 0, 0, .5);\n\
-    }\n\
-    td.subtext a:link, td.title a:link, \n\
-    td.subtext a:visited, td.title a:visited {\n\
-        color: rgba(0, 0, 0, .8);\n\
-    }\n\
-    td.subtext a:visited, td.title a:visited {\n\
-        text-decoration: line-through !important;\n\
-    }';
-    $('<style>').text(style_text).appendTo('head')
     $scores.each(function() {
         var $this = $(this);
         var score = parseInt($this.text(), 10);
